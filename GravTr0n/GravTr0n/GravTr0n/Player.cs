8e2459e7dee16ce79018830bea688553aca394e9
Pål Marius Sivertsen <palmarius@hotmail.com>
2013-04-10 19:52:18 +0200

    Added Player
    New Sheit n shit

---
+++ GravTr0n/GravTr0n/GravTr0n/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace GravTr0n
+{
+    public class Player
+    {
+        public Vector2 Position
+        {
+            get { return Position; }
+            set { Position = value; }
+        }
+
+        public Rectangle CollitionRectangle
+        {
+            get { return CollitionRectangle; }
+            set { CollitionRectangle = value; }
+        }
+    }
+}
