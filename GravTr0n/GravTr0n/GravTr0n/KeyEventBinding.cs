af989a2a6e9774efa58c0ca922d810f5382d5e51
Morten A. Salvesen <msalvefirm@gmail.com>
2013-04-11 21:24:18 +0200

    ASDFGH INPUT FFS!
    penispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenispenis
    8=====================B~~~~~~~~~

---
+++ GravTr0n/GravTr0n/GravTr0n/KeyEventBinding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace GravTr0n
+{
+    struct KeyEventBinding
+    {
+        public Keys key;
+        public Events eventName;
+
+        public KeyEventBinding(Keys keys, Events keyEvent) 
+        { 
+            key = keys; 
+            eventName = keyEvent; 
+        }
+    }
+}
