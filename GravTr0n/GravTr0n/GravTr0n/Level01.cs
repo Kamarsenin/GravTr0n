b768e57e2d7f318980fe8765b002a3e6665d54a5
Pål Marius Sivertsen <palmarius@hotmail.com>
2013-04-12 17:44:21 +0200

    Collition fix

--- GravTr0n/GravTr0n/GravTr0n/Level01.cs
+++ GravTr0n/GravTr0n/GravTr0n/Level01.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace GravTr0n
 {
@@ -45,13 +50,14 @@ namespace GravTr0n
 
         }
 
-        public void Update(ICollisionService collisionManager)
+        public void Update(ICollisionService collisionManager, Vector2 offset)
         {
             for (int i = 1; i < _levelObjects.Count; i++)
             {
                 if (collisionManager.IsCollision(_player, _levelObjects[i]))
                 {
-                    collisionManager.ResolveCollision(_player, _levelObjects[i]);
+                    _player.Position = _player.PrevPosition;
+                    _player.Velocity = Vector2.Zero;
                 }
             }
         }
