46a41b1a474a7ba64c14c40850f7a96c366a59dd
DanRJ <johdan12@nith.no>
2013-04-12 13:26:11 +0200

    Fikset knapper littegranne
    Granne

--- GravTr0n/GravTr0n/GravTr0n/KeyBindingsMenu.cs
+++ GravTr0n/GravTr0n/GravTr0n/KeyBindingsMenu.cs
@@ -18,40 +18,41 @@ namespace GravTr0n
         private AnimatedDrawable _restart;
         private int dividedPosScreenX = 2;
         private int dividedPosScreenY = 3;
-
+        private int seperateSprites = 47;
+        private int offSetY = 47;
 
         public GameState GameState { get; set; }
         public int GameStateCheck { get; set; }
 
         public KeyBindingsMenu(Texture2D keyBindingArt, int screenWidth, int screenHeight, GameState gameState, int gameStateCheck)
         {
-            Rectangle _keysRect = new Rectangle(0, 0, 128, 94);
+            Rectangle _keysRect = new Rectangle(0, 0, 128, 46);
             _keys = new AnimatedDrawable(keyBindingArt, 4, _keysRect, 0.2f);
-            _keys.Position = new Vector2(screenWidth / dividedPosScreenX - 128, screenHeight / dividedPosScreenY - 94);
+            _keys.Position = new Vector2(screenWidth / dividedPosScreenX - 69, screenHeight / dividedPosScreenY - seperateSprites * 2);
 
             _left = new AnimatedDrawable(keyBindingArt, 4, _keysRect, 0.1f);
-            _left.Position = new Vector2(_keys.Position.X, _keys.Position.Y + 94);
-            _left.StartingOffset = new Point(_left.StartingOffset.X, 94);
+            _left.Position = new Vector2(_keys.Position.X, _keys.Position.Y + seperateSprites);
+            _left.StartingOffset = new Point(_left.StartingOffset.X, offSetY);
 
             _right = new AnimatedDrawable(keyBindingArt, 4, _keysRect, 0.1f);
-            _right.Position = new Vector2(_left.Position.X, _left.Position.Y + 94);
-            _right.StartingOffset = new Point(_right.StartingOffset.X, 188);
+            _right.Position = new Vector2(_left.Position.X, _left.Position.Y + seperateSprites);
+            _right.StartingOffset = new Point(_right.StartingOffset.X, _left.StartingOffset.Y + offSetY);
 
             _jump = new AnimatedDrawable(keyBindingArt, 4, _keysRect, 0.1f);
-            _jump.Position = new Vector2(_right.Position.X, _right.Position.Y + 94);
-            _jump.StartingOffset = new Point(_jump.StartingOffset.X, 282);
+            _jump.Position = new Vector2(_right.Position.X, _right.Position.Y + seperateSprites);
+            _jump.StartingOffset = new Point(_jump.StartingOffset.X, _right.StartingOffset.Y + offSetY);
 
             _exit = new AnimatedDrawable(keyBindingArt, 4, _keysRect, 0.1f);
-            _exit.Position = new Vector2(_jump.Position.X, _jump.Position.Y + 94);
-            _exit.StartingOffset = new Point(_exit.StartingOffset.X, 376);
+            _exit.Position = new Vector2(_jump.Position.X, _jump.Position.Y + seperateSprites);
+            _exit.StartingOffset = new Point(_exit.StartingOffset.X, _jump.StartingOffset.Y + offSetY);
 
             _pause = new AnimatedDrawable(keyBindingArt, 4, _keysRect, 0.1f);
-            _pause.Position = new Vector2(_exit.Position.X, _exit.Position.Y + 94);
-            _pause.StartingOffset = new Point(_pause.StartingOffset.X, 470);
+            _pause.Position = new Vector2(_exit.Position.X, _exit.Position.Y + seperateSprites);
+            _pause.StartingOffset = new Point(_pause.StartingOffset.X, _exit.StartingOffset.Y + offSetY);
 
             _restart = new AnimatedDrawable(keyBindingArt, 4, _keysRect, 0.1f);
-            _restart.Position = new Vector2(_pause.Position.X, _pause.Position.Y + 94);
-            _restart.StartingOffset = new Point(_restart.StartingOffset.X, 564);
+            _restart.Position = new Vector2(_pause.Position.X, _pause.Position.Y + seperateSprites);
+            _restart.StartingOffset = new Point(_restart.StartingOffset.X, _pause.StartingOffset.Y + offSetY);
 
             GameState = gameState;
             GameStateCheck = gameStateCheck;
@@ -96,12 +97,12 @@ namespace GravTr0n
         private void MouseClicked(int x, int y)
         {
             Rectangle mouseClickRect = new Rectangle(x, y, 10, 10);
-            Rectangle leftRect = new Rectangle((int)_left.Destination.X, (int)_left.Destination.Y, 128, 94);
-            Rectangle rightRect = new Rectangle((int)_right.Destination.X, (int)_right.Destination.Y, 128, 94);
-            Rectangle jumpRect = new Rectangle((int)_jump.Destination.X, (int)_jump.Destination.Y, 128, 94);
-            Rectangle exitRect = new Rectangle((int)_exit.Destination.X, (int)_exit.Destination.Y, 128, 94);
-            Rectangle pauseRect = new Rectangle((int)_pause.Destination.X, (int)_pause.Destination.Y, 128, 94);
-            Rectangle restartRect = new Rectangle((int)_restart.Destination.X, (int)_restart.Destination.Y, 128, 94);
+            Rectangle leftRect = new Rectangle((int)_left.Destination.X, (int)_left.Destination.Y, 128, 46);
+            Rectangle rightRect = new Rectangle((int)_right.Destination.X, (int)_right.Destination.Y, 128, 46);
+            Rectangle jumpRect = new Rectangle((int)_jump.Destination.X, (int)_jump.Destination.Y, 128, 46);
+            Rectangle exitRect = new Rectangle((int)_exit.Destination.X, (int)_exit.Destination.Y, 128, 46);
+            Rectangle pauseRect = new Rectangle((int)_pause.Destination.X, (int)_pause.Destination.Y, 128, 46);
+            Rectangle restartRect = new Rectangle((int)_restart.Destination.X, (int)_restart.Destination.Y, 128, 46);
 
             if (mouseClickRect.Intersects(leftRect))
             {
